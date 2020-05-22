873b1afb801aa97e476983e48665227958b6d23e
Michael Kilesa <kilesa@gmail.com>
2020-05-22 17:45:38 +0300

    initial login effects tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 describe('LoginComponent', () => {
+
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule, MatCheckboxModule],
+      providers: [
+        { provide: Store, useValue: { select: () => of({}) } }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +29,26 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have its form invalid when empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+
+    const email = component.form.controls.email;
+    expect(email.valid).toBeFalsy();
+
+    email.setValue('');
+    expect(email.hasError('required')).toBeTruthy();
+
+    email.setValue('a');
+    expect(email.hasError('email')).toBeTruthy();
+    expect(email.hasError('required')).toBeFalsy();
+
+    email.setValue('a@b.com');
+    expect(email.hasError('email')).toBeFalsy();
+
+  });
+
 });
---
+++ src/app/login/login.effects.spec.ts
@@ -0,0 +1,107 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions, } from '@ngrx/effects/testing';
+import { ApiService } from '../_core/api/api.service';
+import { of, Observable, throwError } from 'rxjs';
+import { LoginEffects } from './login.effects';
+import { Action } from '@ngrx/store';
+import { login, loginError, /* signup */ } from './login.constants';
+import { readToken } from '../app.constants';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
+describe('LoginEffects', () => {
+
+  let effects: LoginEffects;
+  let actions$: Observable<Action>;
+  let router: Router;
+
+  const apiServiceSpy = jasmine.createSpyObj('ApiService', ['request', 'endpoint']);
+
+  beforeEach(() => {
+
+    spyOn(localStorage, 'setItem');
+
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ],
+      providers: [
+        { provide: ApiService, useValue: apiServiceSpy },
+        LoginEffects,
+        provideMockActions(() => actions$)
+      ],
+    });
+    effects = TestBed.inject(LoginEffects);
+    router = TestBed.inject(Router);
+
+  });
+
+  describe('login$', () => {
+
+    beforeEach(() => {
+      actions$ = of(login({
+        payload: {
+          email: 'email',
+          password: 'password',
+          rememberMe: true
+        }
+      }));
+    });
+
+    it('should dispatch readToken and tap on success', () => {
+      const navigateSpy = spyOn(router, 'navigate');
+      apiServiceSpy.request.and.returnValue(of({ login: 'token' }));
+      effects.login$.subscribe(res => {
+        expect(navigateSpy).toHaveBeenCalledWith(['/']);
+        expect(localStorage.setItem).toHaveBeenCalledWith('auth_token', '"token"');
+        expect(res).toEqual(readToken());
+      });
+    });
+
+    it('should dispatch error on error', () => {
+      const navigateSpy = spyOn(router, 'navigate');
+      apiServiceSpy.request.and.returnValue(throwError({ error: { message: { error: 'whatever' } } }));
+      effects.login$.subscribe((res) => {
+        expect(navigateSpy).not.toHaveBeenCalled();
+        expect(localStorage.setItem).not.toHaveBeenCalled();
+        expect(res).toEqual(loginError({ error: 'whatever' }));
+      });
+    });
+
+  });
+
+
+
+  /* it('should dispatch readToken and tap on signup effect', () => {
+    actions$ = of(signup({
+      payload: {
+        email: 'email',
+        password: 'password',
+        phone: 'phone',
+      }
+    }));
+    apiServiceSpy.request.and.returnValue(of({ account: 'token' }));
+    effects.signup$.subscribe(res => {
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+      expect(localStorage.setItem).toHaveBeenCalledWith('auth_token', '"token"');
+      expect(res).toEqual(readToken());
+    });
+  });
+
+  it('should tap on recovery effect', () => {
+    actions$ = of(signup({
+      payload: {
+        email: 'email',
+        password: 'password',
+        phone: 'phone',
+      }
+    }));
+    apiServiceSpy.request.and.returnValue(of({ account: 'token' }));
+    effects.signup$.subscribe(res => {
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+      expect(localStorage.setItem).toHaveBeenCalledWith('auth_token', '"token"');
+      expect(res).toEqual(readToken());
+    });
+  }); */
+
+});
