6fa484cb534e0b826d18d8c08796f3494911fe78
Michael Kilesa <kilesa@gmail.com>
2020-05-22 19:51:40 +0300

    login-guard full test

--- src/app/login/login-guard.service.spec.ts
+++ src/app/login/login-guard.service.spec.ts
@@ -1,14 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { LoginGuard } from './login-guard.service';
-import { Store } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { loginReset } from './login.constants';
-import { Login } from './login.types';
+import { Store } from '@ngrx/store';
 
 describe('LoginGuard', () => {
 
   let service: LoginGuard;
-  let store: Store<Login>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,17 +21,26 @@ describe('LoginGuard', () => {
       ]
     });
     service = TestBed.inject(LoginGuard);
-    store = TestBed.inject(Store);
+    store = TestBed.inject(Store) as MockStore;
   }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should invoke loginReset on canActivate', () => {
+  it('should invoke loginReset on canActivate when there is an error', () => {
     spyOn(store, 'dispatch');
     service.canActivate().subscribe(res => {
       expect(store.dispatch).toHaveBeenCalledWith(loginReset());
+      expect(res).toBeTruthy();
+    }, console.error);
+  });
+
+  it('should not invoke loginReset on canActivate when there is\'nt an error', () => {
+    store.setState({ login: { error: '' } });
+    spyOn(store, 'dispatch');
+    service.canActivate().subscribe(res => {
+      expect(store.dispatch).not.toHaveBeenCalled();
       expect(res).toBeTruthy();
     }, console.error);
   });
