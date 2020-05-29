61c2b99eba8e15bd705750ffa66cd0b2e2ff6bdf
Michael Kilesa <kilesa@gmail.com>
2020-05-29 13:15:37 +0300

    prospect setup

--- src/app/autobot/autobot.component.ts
+++ src/app/autobot/autobot.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormGroupT, formCreate, formPatchValue } from '../_core/form/form';
-import { FormBuilder } from '@angular/forms';
 import { Autobot } from './autobot.types';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectAutobot } from './autobot.model';
 import { loadSuggestedActivities } from './autobot.constants';
 import { slideInOut } from '../_core/animations/animations';
-import { ChartType } from 'ng-chartist';
-// import { IChartistData } from 'chartist';
 import { blankProfiilePic } from '../app.constants';
-
-interface EditorForm {
-  expanded: { [key: string]: boolean };
-  snippets: { [key: string]: string };
-}
 
 @Component({
   selector: 'autobound-autobot',
@@ -27,64 +18,14 @@ export class AutobotComponent implements OnInit {
 
   blankProfiilePic = blankProfiilePic;
   model$!: Observable<Autobot>;
-  form!: FormGroupT<EditorForm>;
-  type: ChartType = 'Pie';
-  chartOptions!: any;
-  chartData!: any; //IChartistData;
 
   constructor(
-    private fb: FormBuilder,
     private store: Store,
   ) { }
 
   ngOnInit() {
     this.model$ = this.store.select(selectAutobot);
     this.store.dispatch(loadSuggestedActivities());
-    this.form = formCreate<EditorForm>(this.fb, {
-      expanded: [{}],
-      snippets: [{
-        1: '<h2>Autobot here</h2><h3>Subject: Monthly travel news and inspiration</h3>',
-        2: '<h3>Destination of the Month</h3><h4>Valletta</h4><p>The capital city of <a href="https://en.wikipedia.org/wiki/Malta" target="_blank" rel="external"> Malta </a> is the top destination this summer. It’s home to a cutting-edge contemporary architecture, baroque masterpieces, delicious local cuisine and at least 8 months of sun.It’s also a top destination for filmmakers, so you can take a tour through locations familiar to you from Game of Thrones, Gladiator, Troy and many more.</p>',
-        3: '<h3>The three greatest things you learn from traveling</h3><p><a href="#">Find out more</a></p>',
-        4: '<h3>Walking the capitals of Europe: Warsaw</h3><p><a href="#">Find out more</a></p',
-      }],
-    });
-
-
-    this.chartOptions = {
-      donut: true,
-      donutWidth: 40,
-      donutSolid: true,
-      startAngle: 270,
-      showLabel: false,
-    };
-
-    this.chartData = {
-      series: [{value: 20, className: 'series1'}, 10, 30, 40],
-    };
-
-  }
-
-  changeData() {
-    formPatchValue(this.form, {
-      snippets: {
-        ...this.form.controls.snippets.value,
-        3: '<h2>Some additional text</h3>'
-      }
-    });
-  }
-
-  getData(value: EditorForm) {
-    alert(JSON.stringify(value));
-  }
-
-  toggleItem(id: string) {
-    formPatchValue(this.form, {
-      expanded: {
-        ...this.form.controls.expanded.value,
-        [id]: !this.form.controls.expanded.value[id]
-      }
-    });
   }
 
 }
--- src/app/autobot/prospect/prospect.component.ts
+++ src/app/autobot/prospect/prospect.component.ts
@@ -3,23 +3,80 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectRouteParam } from 'src/app/app.model';
 import { App } from 'src/app/app.types';
+import { FormGroupT, formCreate, formPatchValue } from '../../_core/form/form';
+import { FormBuilder } from '@angular/forms';
+import { ChartType } from 'ng-chartist';
+
+interface EditorForm {
+  expanded: { [key: string]: boolean };
+  snippets: { [key: string]: string };
+}
 
 @Component({
   selector: 'autobound-prospect',
   templateUrl: './prospect.component.html',
-  styleUrls: ['./prospect.component.scss'],
+  styleUrls: ['../autobot.component.scss', './prospect.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProspectComponent implements OnInit {
 
   currentRoute$!: Observable<any>;
+  form!: FormGroupT<EditorForm>;
+  chartOptions!: any;
+  chartData!: any; // IChartistData;
+  type: ChartType = 'Pie';
 
   constructor(
     private store: Store<App>,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
     this.currentRoute$ = this.store.select(selectRouteParam('prospectId'));
+    this.form = formCreate<EditorForm>(this.fb, {
+      expanded: [{}],
+      snippets: [{
+        1: '<h2>Autobot here</h2><h3>Subject: Monthly travel news and inspiration</h3>',
+        2: '<h3>Destination of the Month</h3><h4>Valletta</h4><p>The capital city of <a href="https://en.wikipedia.org/wiki/Malta" target="_blank" rel="external"> Malta </a> is the top destination this summer. It’s home to a cutting-edge contemporary architecture, baroque masterpieces, delicious local cuisine and at least 8 months of sun.It’s also a top destination for filmmakers, so you can take a tour through locations familiar to you from Game of Thrones, Gladiator, Troy and many more.</p>',
+        3: '<h3>The three greatest things you learn from traveling</h3><p><a href="#">Find out more</a></p>',
+        4: '<h3>Walking the capitals of Europe: Warsaw</h3><p><a href="#">Find out more</a></p',
+      }],
+    });
+
+
+    this.chartOptions = {
+      donut: true,
+      donutWidth: 40,
+      donutSolid: true,
+      startAngle: 270,
+      showLabel: false,
+    };
+
+    this.chartData = {
+      series: [{ value: 20, className: 'series1' }, 10, 30, 40],
+    };
+  }
+
+  changeData() {
+    formPatchValue(this.form, {
+      snippets: {
+        ...this.form.controls.snippets.value,
+        3: '<h2>Some additional text</h3>'
+      }
+    });
+  }
+
+  getData(value: EditorForm) {
+    alert(JSON.stringify(value));
+  }
+
+  toggleItem(id: string) {
+    formPatchValue(this.form, {
+      expanded: {
+        ...this.form.controls.expanded.value,
+        [id]: !this.form.controls.expanded.value[id]
+      }
+    });
   }
 
 }
