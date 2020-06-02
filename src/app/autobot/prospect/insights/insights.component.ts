a1c0957324ab8fd5acb163603fd5ad29445ac6cd
Michael Kilesa <kilesa@gmail.com>
2020-06-02 16:31:39 +0300

    activities, opportunities

--- src/app/autobot/prospect/insights/insights.component.ts
+++ src/app/autobot/prospect/insights/insights.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { faEnvelope, faPhone, faHandshake } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'autobound-insights',
@@ -8,11 +9,42 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class InsightsComponent implements OnInit {
 
+  fa = { faEnvelope, faPhone, faHandshake };
   foods: any[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
   ];
+  @Input() activities = [{
+    date: '2 Apr, 2020 6:39 PM',
+    from: 'Ivan Poddubchenko',
+    to: 'Richard Hunter',
+    caption: 'Just read article on SwiftStack\'s purchase, congrats!',
+    text: 'I have a few suggestions for capitalizing on the latest funding round…',
+    responses: 1,
+  }, {
+    date: '2 Apr, 2020 6:59 PM',
+    from: 'Ivan Poddubchenko',
+    to: 'Richard Hunter',
+    caption: 'Just read article on SwiftStack\'s purchase, congrats!',
+    text: 'I have a few suggestions for capitalizing on the latest funding round…',
+    responses: 1,
+  }];
+  @Input() opportunities = [{
+    date: '2 Apr, 2020 6:39 PM',
+    closeDate: '10 May, 2020',
+    owner: 'Richard Hunter',
+    caption: 'Inbound from site - Microsoft, Richard Hunter',
+    text: 'This is description of this opportunity. It can be a few sentences. Now it’s just text for example.',
+    valuation: '$9 500',
+  }, {
+    date: '2 Apr, 2020 6:59 PM',
+    closeDate: '11 May, 2020',
+    owner: 'Richard Hunter',
+    caption: 'Inbound from site - Microsoft, Richard Hunter',
+    text: 'This is description of this opportunity. It can be a few sentences. Now it’s just text for example.',
+    valuation: '$500',
+  }];
 
   constructor() { }
 
